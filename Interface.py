202f77876876d7f59dd662c2433bdfc867b553f7
Kekkerinotaco <88616570+Kekkerinotaco@users.noreply.github.com>
2021-12-08 23:46:37 +0300

    Start_file.py -> Interface.py
    Предлагаю произвести создание интерфейса при помощи ткинкера, почти во всех гайдах для нубасов используется именно он.

---
+++ Interface.py
@@ -0,0 +1,8 @@
+# Creating the interface
+
+from tkinter import *
+ 
+window = Tk()
+window.title("KrasNIPI in collaboration with GPN snake game for $1000000000")
+ 
+window.mainloop()
--- Start_file.py
+++
@@ -1,1 +0,0 @@
-# Some start file 
