a05e226fa8628744f4efcdf47d01c62c41342ffe
GYMoroz <grighoriim@mail.ru>
2021-12-26 14:10:03 +0300

    Работаю по сайту: http://pythonicway.com/python-games/python-arcade/18-python-snake
    В этом коммите доработал интерфейс, теперь выводится окно, нужного цвета

--- Interface.py
+++ Interface.py
@@ -1,8 +1,16 @@
 # Creating the interface
 
 from tkinter import *
- 
+
 window = Tk()
 window.title("KrasNIPI in collaboration with GPN snake game for $1000000000")
- 
+
+WIDTH = 800
+HEIGHT = 600
+SEG_SIZE = 20
+IN_GAME = True
+
+c = Canvas(window, width=WIDTH, height=HEIGHT, bg="#555555")
+c.grid()
+c.focus_set()
 window.mainloop()
